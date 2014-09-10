4dcf8ddc16bb4cd0b4454acaf079e58470122ba2
carriexu <carrielovesfood@gmail.com>
2014-09-09 20:01:40 -0400

    session for facebook and instagram now works

--- app.rb
+++ app.rb
@@ -51,12 +51,12 @@ class App < Sinatra::Base
   WUNDERGROUND_API_KEYS = "414d6ac14863ad60"
   INSTAGRAM_CLIENT_ID = "b668170700ab4a2c8793bdcfcc875806"
   INSTAGRAM_CLIENT_SECRET = "175b08336c364ea18013ed373dd96f0b"
-  INSTAGRAM_REDIRECT_URL = "http://infinite-spire-5264.herokuapp.com/oauth_callback/instagram"
+  INSTAGRAM_REDIRECT_URL = "http://127.0.0.1:9292/oauth_callback/instagram"
   # could not get below Instagram access token to work as a constant
   # INSTAGRAM_ACCESS_TOKEN = "391569309.b668170.c4cf70355fa4463690d0264ab3ce3d26"
   FACEBOOK_CLIENT_ID = "620638748048605"
   FACEBOOK_CLIENT_SECRET ="27b3f2001171930dcf0c475124972b12"
-  FACEBOOK_REDIRECT_URL = "http://infinite-spire-5264.herokuapp.com/oauth_callback/facebook"
+  FACEBOOK_REDIRECT_URL = "http://127.0.0.1:9292/oauth_callback/facebook"
 
   ########################
   # Routes
@@ -73,8 +73,8 @@ class App < Sinatra::Base
     facebook_state = SecureRandom.urlsafe_base64
     # storing state in session because we need to compare it in a later request
     # the following does not work, cannot set session[:state] to both instagram_state and facebook_state
-    # session[:state] = instagram_state
-    # session[:state] = facebook_state
+    session[:instagram_state] = instagram_state
+    session[:facebook_state] = facebook_state
 
     @instagram_uri = "#{instagram_base_url}?client_id=#{INSTAGRAM_CLIENT_ID}&redirect_uri=#{INSTAGRAM_REDIRECT_URL}&response_type=code&state=#{instagram_state}"
     @facebook_uri = "#{facebook_base_url}?client_id=#{FACEBOOK_CLIENT_ID}&redirect_uri=#{FACEBOOK_REDIRECT_URL}&response_type=code&state=#{facebook_state}"
@@ -87,7 +87,7 @@ class App < Sinatra::Base
     # state = params[:state]
     code = params[:code]
     # compare the states to ensure the information is from who we think it is
-    if session[:state] == params[:state]
+    if session[:instagram_state] == params[:state]
       instagram_response = HTTParty.post("https://api.instagram.com/oauth/access_token",
                               :body => {
                               client_id: INSTAGRAM_CLIENT_ID,
@@ -108,7 +108,7 @@ class App < Sinatra::Base
   get('/oauth_callback/facebook') do
     code = params[:code]
 
-    if session[:state] == params[:state]
+    if session[:facebook_state] == params[:state]
       facebook_response = HTTParty.post("https://graph.facebook.com/oauth/access_token",
                               :body => {
                                 client_id: FACEBOOK_CLIENT_ID,
